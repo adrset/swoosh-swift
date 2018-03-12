696a5569bec059a330d9262af5266e2c41f8df65
Adrian Setniewski <274433@pw.edu.pl>
2018-03-12 12:09:31 +0100

    Programmatic segues and code refactoring.

--- Shoosh/Shoosh/Controller/WelcomeVC.swift
+++ Shoosh/Shoosh/Controller/WelcomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImage: UIImageView!
@@ -23,5 +23,9 @@ class ViewController: UIViewController {
         
     }
 
+    @IBAction func onGetStartedTapped(_ sender: Any) {
+        performSegue(withIdentifier: "leagueVCSegue", sender: self)
+    }
+    
 }
 
